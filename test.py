fcd4e33d4628e295cfb2b3f2ff9b5e5081c89af0
Ethan Troy <63926014+ethanolivertroy@users.noreply.github.com>
2023-02-11 12:09:02 -0500

    Create test.py

---
+++ test.py
@@ -0,0 +1,46 @@
+import argparse
+import nmap
+import openai
+
+# Set OpenAI API key
+openai.api_key = "your_openai_api_key"
+
+# Define the command line arguments
+parser = argparse.ArgumentParser(description='Nmap Network Scanner with OpenAI Analysis')
+parser.add_argument('host', type=str, help='Host or IP address to scan')
+parser.add_argument('-p', '--port', type=str, help='Port or port range to scan')
+
+# Parse the command line arguments
+args = parser.parse_args()
+
+# Initialize Nmap
+nm = nmap.PortScanner()
+
+# Perform the Nmap scan
+nm.scan(args.host, args.port)
+
+# Extract the results of the Nmap scan
+for host in nm.all_hosts():
+    print('Host: {} ({})'.format(host, nm[host].hostname()))
+    print('State: {}'.format(nm[host].state()))
+    for protocol in nm[host].all_protocols():
+        print('Protocol: {}'.format(protocol))
+        lport = nm[host][protocol].keys()
+        lport.sort()
+        for port in lport:
+            print('port: {}\tstate: {}'.format(port, nm[host][protocol][port]['state']))
+
+# Send the results to the OpenAI API
+prompt = "Generate a report based on the Nmap scan results for host: {}".format(host)
+response = openai.Completion.create(
+    engine="text-davinci-002",
+    prompt=prompt,
+    max_tokens=1024,
+    n=1,
+    stop=None,
+    temperature=0.5,
+)
+
+# Extract the generated report from the OpenAI API response
+text = response["choices"][0]["text"]
+print("OpenAI Report:\n{}".format(text))
